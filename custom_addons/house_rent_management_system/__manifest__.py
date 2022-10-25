742d4e57661215682ac487b08b445e26d92a4382
smjahin <morsalin.jahin@northsouth.edu>
2022-10-25 11:01:11 +0600

    Branch inital commit

---
+++ custom_addons/house_rent_management_system/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'House Rent Management System',
+    'version': '1.0.0',
+    'category': 'House Rent Management System',
+    'description': """House Rent Management System""",
+    'summary': 'House Rent Management System',
+    'depends': [],
+    'data': [],
+    'demo': [],
+    'sequence': -100,
+    'author': 'Abir Hasan',
+    'auto_install': True,
+    'application': True,
+    'installable': True,
+    'license': 'LGPL-3',
+
+}
