3ecc6ec476900b0a2215c3643bdecbd6cb1665b0
smjahin <morsalin.jahin@northsouth.edu>
2022-10-27 00:16:21 +0600

    MOD: <Payment> flat and property field added in payment module;

--- custom_addons/house_rent_management_system/models/payment.py
+++ custom_addons/house_rent_management_system/models/payment.py
@@ -1,10 +1,13 @@
 from odoo import fields, models, api
 
+from odoo.exceptions import ValidationError
 
 class Payment(models.Model):
     _name = 'payment.payment'
     _description = 'Payment Information'
     _rec_name = 'renter_id'
+    _rec_name = 'flat_id'
+    _rec_name = 'property_id'
 
     renter_id = fields.Many2one('property.renter', string='Renter Name')
     month = fields.Selection([('january', 'January'), ('february', 'February'),
@@ -12,7 +15,8 @@ class Payment(models.Model):
                               ('jun', 'June'),('july', 'July'),('august', 'August'),
                               ('september', 'September'),('october', 'October'),('november', 'November'),
                               ('december', 'December')], string='Month')
-    flat_id = fields.Char(string='Flat')
+    flat_id = fields.Many2one('property.flats', string='Flat Name')
+    property_id = fields.Many2one('property.property', string='Property Name')
     year = fields.Char(string='Year')
     amount = fields.Float(string='Amount')
     state = fields.Selection([
@@ -28,3 +32,11 @@ class Payment(models.Model):
         self.state = 'pending'
     def action_cancelled(self):
         self.state = 'cancelled'
+
+    # @api.onchange('flat_id')
+    # def if_already_paid(self):
+    #     if self.flat_id:
+    #         flat = self.env['property.renter'].search([('flat_id', '=', self.flat_id.id)])
+    #         flat_name = self.flat_id.flat_name
+    #         if flat:
+    #             raise ValidationError(f'This {flat_name} Flat Already Rented')
