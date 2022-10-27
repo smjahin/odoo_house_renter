c54f306406c7fbf5a08d7c5c555b71db64bb7469
smjahin <morsalin.jahin@northsouth.edu>
2022-10-28 02:19:42 +0600

    ADD: <Property> Flat count under a property added;

--- custom_addons/house_rent_management_system/models/property.py
+++ custom_addons/house_rent_management_system/models/property.py
@@ -4,13 +4,15 @@ from odoo.exceptions import ValidationError
 class Property(models.Model):
     _name = 'property.property'
     _description = 'All Property'
-    _rec_name = 'manager_id'
+    _rec_name = 'property_name'
 
 
     location_id = fields.Many2one('house.location', string='Location Info.')
     property_name = fields.Char(string='Property Name')
     manager_id = fields.Many2one('manager.information', string='Manager Name')
     total_flat = fields.Integer(string='Total Flat')
+    rented_flat = fields.Integer(string='Rented Flat', compute='_compute_appointment_count')
+    total_amount = fields.Float(string='Total amount', compute='_compute_property_amount')
 
     @api.onchange('manager_id')
     def verify_property(self):
@@ -19,3 +21,18 @@ class Property(models.Model):
             manager_name = self.manager_id.name
             if manager_info:
                 raise ValidationError(f'This {manager_name} is already tagged with another property')
+
+
+    def _compute_appointment_count(self):
+        # rented_flat_count = self.env['property.flats'].search_count([('property_id','=',self.id)])
+        rented_flat_count = self.env['property.flats'].search_count([
+                                ('property_id', '=', self.id)
+                            ])
+        self.rented_flat=rented_flat_count
+
+    # def _compute_property_amount(self):
+    #     manager_info = self.env['property.flats'].search([('property_id', '=', self.id)])
+    #     for m in manager_info:
+    #         print(m)
+    #     self.total_amount = manager_info
+
