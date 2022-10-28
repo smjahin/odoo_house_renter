c9299598a2c602c420d743d7f5468ca3b01ee12c
smjahin <morsalin.jahin@northsouth.edu>
2022-10-28 09:48:36 +0600

    ADD: <Renter Ledger> Renter wise total flat paid amount added;

--- custom_addons/house_rent_management_system/models/renter_ledger.py
+++ custom_addons/house_rent_management_system/models/renter_ledger.py
@@ -16,8 +16,8 @@ class RenterLedger(models.Model):
                               ('september', 'September'), ('october', 'October'), ('november', 'November'),
                               ('december', 'December')], string='Month')
     flat_rent = fields.Float( string='Flat Rent', related='flat_id.price')
-    flat_paid_amount = fields.Float(string='Flat Paid Amount')
-    flat_due_amount = fields.Float(string='Falt Due Amount')
+    flat_paid_amount = fields.Float(string='Flat Paid Amount', compute='_compute_flat_paid_amount')
+    flat_due_amount = fields.Float(string='Falt Due Amount', compute='_compute_flat_due_amount')
 
     @api.onchange('property_id')
     def _onchange_property_id_wrapper(self):
@@ -28,7 +28,16 @@ class RenterLedger(models.Model):
         return res
 
     def _compute_flat_paid_amount(self):
-        pass
+        for rec in self:
+            faid_amount_info = self.env['payment.payment'].search([('flat_id', '=', rec.flat_id.id)
+                                                                  ,('month', '=', rec.month), ('year','=',rec.year)
+                                                               ,('state','=','paid')])
+            sum = 0
+            for m in faid_amount_info:
+                sum = sum + m.amount
+            rec.flat_paid_amount = sum
 
     def _compute_flat_due_amount(self):
+        # for rec
+        # self.flat_due_amount = self.flat_rent - self.flat_paid_amount
         pass
